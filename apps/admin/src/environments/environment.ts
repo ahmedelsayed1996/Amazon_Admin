5a067c2e6d0c2bed2e3044f0c4abf03a2e8b8cc7
Ahmed El-Ged <121124311+ahmedelsayed1996@users.noreply.github.com>
2023-04-06 20:57:30 +0200

    first update

---
+++ apps/admin/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    firebase :{
+       apiKey: "AIzaSyBYQa7SBSK05ZnMnzqNMmTaKDN5abVHkFA",
+       authDomain: "admin-47ec4.firebaseapp.com",
+       projectId: "admin-47ec4",
+       storageBucket: "admin-47ec4.appspot.com",
+       messagingSenderId: "825377216025",
+       appId: "1:825377216025:web:4cae2e6e58c070fbd03c88",
+       measurementId: "G-5H2N6391SL"
+     },
+     production: false
+     
+     // Initialize Firebase
+};
